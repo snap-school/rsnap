4dce8d523918be3a392d730286ea452873b91d5c
Claessens Simon <gagalago@gmail.com>
2014-03-05 11:06:09 +0100

    init paperclip s3

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+unless Rails.env.development?
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+end
