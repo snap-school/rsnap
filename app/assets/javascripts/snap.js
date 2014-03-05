df84380ed28c99afabdab1b2d35e3740047970b1
Claessens Simon <gagalago@gmail.com>
2014-03-05 16:52:26 +0100

    add item on snap robot menu

--- app/assets/javascripts/snap.js
+++ app/assets/javascripts/snap.js
@@ -2,6 +2,7 @@ var world;
 window.addLoadEvent(function () {
   world = new WorldMorph(document.getElementById('world'));
   world.role = 1;//TODO chose the good one and make them more explicit with string instead of int
+  //role: 0=>student 1=>teacher
   //world.isDevMode = true;
   ide = new IDE_Morph();
   ide.openIn(world);
