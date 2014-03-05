84db10b0ff1a1a395bb4ce5964e5af62cd1c44a8
Claessens Simon <gagalago@gmail.com>
2014-03-05 11:30:58 +0100

    add migrztion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226084803) do
+ActiveRecord::Schema.define(version: 20140305102953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20140226084803) do
     t.datetime "source_code_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "mission_order",            default: 0
-    t.string   "small_description",        default: ""
+    t.integer  "mission_order"
+    t.text     "small_description"
   end
 
   create_table "programs", force: true do |t|
