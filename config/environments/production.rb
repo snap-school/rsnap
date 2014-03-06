f63b5d058bb126dd9eee2a60e33d7cf1bf87213f
Claessens Simon <gagalago@gmail.com>
2014-03-06 16:36:21 +0100

    s3 mixed content

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rsnap::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_protocol => :https
   }
 end
