58ce191e7d5a88f79f3af1bf5ba97a853dd0c50b
Claessens Simon <gagalago@gmail.com>
2014-03-24 19:00:23 +0100

    open in fr fix #11

--- app/assets/javascripts/snap.js
+++ app/assets/javascripts/snap.js
@@ -3,14 +3,14 @@ window.addLoadEvent(function () {
   world = new WorldMorph(document.getElementById('world'));
 
   world.role = document.querySelector('#world').dataset.role;
-  //role: 0=>student 1=>teacher
   world.isDevMode = false;
+
   ide = new IDE_Morph();
+  ide.openIn(world);
 
   if (document.querySelector('#world').dataset.file) {
     ide.openProjectString(window.getURL(document.querySelector('#world').dataset.file));
   };
-  ide.openIn(world);
 
   setInterval(function() {world.doOneCycle();}, 1);
 });
