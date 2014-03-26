d88dd1bdbd4dbb1704d62be62e06bb3be82be8da
Claessens Simon <gagalago@gmail.com>
2014-03-26 10:13:50 +0100

    30 fps

--- app/assets/javascripts/snap.js
+++ app/assets/javascripts/snap.js
@@ -12,5 +12,5 @@ window.addLoadEvent(function () {
     ide.openProjectString(window.getURL(document.querySelector('#world').dataset.file));
   };
 
-  setInterval(function() {world.doOneCycle();}, 17);
+  setInterval(function() {world.doOneCycle();}, 30);
 });
