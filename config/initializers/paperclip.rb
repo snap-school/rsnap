20380cd244461449b7eb062e37fe4a246bfc874e
Simon Hock <simon.hock@hotmail.com>
2015-08-30 16:33:25 +0200

    db seed && paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,8 @@
 unless Rails.env.development?
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  AWS.config(
+    region: 'eu-central-1',
+    s3_signature_version: :v4
+	)
 end
